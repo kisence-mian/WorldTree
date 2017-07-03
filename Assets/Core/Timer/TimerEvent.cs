e1c1152fbec87f5e1aac4aa8d908d64c7c8dcd91
高凯 <kxjhaha@gmail.com>
2017-07-03 20:28:53 +0800

    FC

---
+++ Assets/Core/Timer/TimerEvent.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public struct TimerEvent
+{
+    public string m_timerName ;
+
+    /// <summary>
+    /// 重复调用次数,-1代表一直调用
+    /// </summary>
+    public int m_repeatCount   ;
+    public int m_currentRepeat;
+
+    /// <summary>
+    /// 是否忽略时间缩放
+    /// </summary>
+    public bool m_isIgnoreTimeScale ;
+    public TimerCallBack m_callBack;
+    public object[] m_objs;
+
+    public float m_timerSpace;
+    public float m_currentTimer ;
+
+    public bool m_isDone ;
+    public void Update()
+    {
+        if (m_isIgnoreTimeScale)
+        {
+            m_currentTimer += Time.unscaledDeltaTime;
+        }
+        else
+        {
+            m_currentTimer += Time.deltaTime;
+        }
+
+        if (m_currentTimer >= m_timerSpace)
+        {
+            m_isDone = true;
+        }
+    }
+
+    public void CompleteTimer()
+    {
+        CallBackTimer();
+        
+        if(m_repeatCount>0)
+        {
+            m_currentRepeat++;
+        }
+
+        if (m_currentRepeat != m_repeatCount)
+        {
+            m_isDone = false;
+            m_currentTimer = 0;
+        }
+    }
+
+    public void CallBackTimer()
+    {
+        if (m_callBack != null)
+        {
+            try
+            {
+                m_callBack(m_objs);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
+        }
+    }
+
+    public void ResetTimer()
+    {
+        m_currentTimer = 0;
+        m_currentRepeat = 0;
+    }
+}
+
+public delegate void TimerCallBack(params object[] l_objs);
